759982b5eb07451dd169593b99b84679d3f8593f
devendev <devendev770@gmail.com>
2026-08-23 20:40:20 +0530

    Time: 0 ms (100%), Space: 14 MB (67.83%) - LeetHub

---
+++ 1927-sum-game/1927-sum-game.cpp
@@ -0,0 +1,37 @@
+#include <string>
+
+class Solution {
+public:
+    bool sumGame(std::string num) {
+        int n = num.length();
+        int mid = n / 2;
+        
+        int sum1 = 0, sum2 = 0;
+        int count1 = 0, count2 = 0;
+        
+       
+        for (int i = 0; i < mid; ++i) {
+            if (num[i] == '?') {
+                count1++;
+            } else {
+                sum1 += num[i] - '0';
+            }
+        }
+        
+        
+        for (int i = mid; i < n; ++i) {
+            if (num[i] == '?') {
+                count2++;
+            } else {
+                sum2 += num[i] - '0';
+            }
+        }
+        
+        
+        if ((count1 + count2) % 2 != 0) {
+            return true;
+        }
+
+        return 2 * (sum1 - sum2) != (count2 - count1) * 9;
+    }
+};
